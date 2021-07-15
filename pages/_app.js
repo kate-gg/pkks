385db28a61bae622326a3a3d55eda65c882b5210
Kate <kate.kun69@gmail.com>
2021-07-15 13:35:09 +0700

    lolxd

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
-
+import 'tailwindcss/tailwind.css'
+import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+ return <>
+  <Head>
+   <title>รวมดาราผู้น่ารักที่เป่านกหวีดมาก่อน</title>
+  </Head>
+  <Component {...pageProps} />
+ </>
 }
 
 export default MyApp
